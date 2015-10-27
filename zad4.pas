455e4229615a16b8c15add25f05958eb3ba4a600
sempeck <jarek.sepek@mediorama.pl>
2015-10-27 20:55:29 +0100

    zadanie 4

---
+++ zad4.pas
@@ -0,0 +1,32 @@
+program zad4;
+
+var
+  i, n : Integer;
+  suma : Real;
+    tn : Char;
+	
+
+begin
+  
+  repeat
+
+		  suma := 0;
+
+		  writeln('Podaj N: ');
+		  readln(n);
+		    
+		  writeln('Elementy:');
+			  for i := 1 to n do 
+			    begin
+			      writeln('1/', i);
+			      suma := suma + 1/i;
+			    end; 
+			 
+		  writeln('Suma ciągu = ', suma:1:2);
+
+	  writeln('Jeszcze raz? (t/n)');
+	  readln(tn);
+
+	until tn = 'n';
+
+end.
