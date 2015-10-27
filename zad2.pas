d3830ca1adf2f93d8f8d2556f2b63fd8bb181e5f
sempeck <jarek.sepek@mediorama.pl>
2015-10-27 19:52:20 +0100

    zadanie 2

---
+++ zad2.pas
@@ -0,0 +1,30 @@
+program zad2;
+
+var
+  i, s, n : Integer;
+  tn : Char;
+
+begin
+
+repeat
+  	 
+  writeln('Podaj liczbe N (od 0 do 10):');
+  readln(n);
+    if (n < 10) and (n >= 0) then
+	  begin
+		  s := 1;
+		  for i := 1 to n-1 do
+		      s := s*(i+1);
+
+		  writeln(n, '! = ', s);
+		  // readln;
+      end    
+    else
+      writeln('Nieprawidlowa liczba.');
+    
+
+  writeln('Jeszcze raz? (t/n)');
+      readln(tn);
+until tn = 'n';
+
+end.
